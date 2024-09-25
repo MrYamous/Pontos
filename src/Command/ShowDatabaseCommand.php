e20da1ab3554106a7450b48d3b78f8383b41146a
MrYamous <matt.lempereur@gmail.com>
2024-09-25 15:34:15 +0200

    continue bundle configuration

--- src/Command/ShowDatabaseCommand.php
+++ src/Command/ShowDatabaseCommand.php
@@ -2,7 +2,7 @@
 
 namespace Yamous\InspectDatabaseBundle\Command;
 
-use Doctrine\DBAL\Connection;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputInterface;
@@ -11,13 +11,12 @@ use Symfony\Component\Console\Output\OutputInterface;
 #[AsCommand(name: 'database:show', aliases: ['db:show'])]
 final class ShowDatabaseCommand extends Command
 {
+
+    public function __construct(public EntityManagerInterface $entityManager) {
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         return Command::SUCCESS;
-    }
-
-    private function getDoctrineConnection(Connection $connection): Connection
-    {
-        return $connection;
     }
 }
--- src/InspectDatabaseBundle.php
+++ src/InspectDatabaseBundle.php
@@ -2,25 +2,40 @@
 
 namespace Yamous\InspectDatabaseBundle;
 
-use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Yamous\InspectDatabaseBundle\Command\ShowDatabaseCommand;
+
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 final class InspectDatabaseBundle extends AbstractBundle
 {
     public function boot(): void
     {
-        //dump($this->container->getParameter('doctrine.default_connection'));
-        //die();
+        dump($this->container->get('doctrine.orm.default_entity_manager'));
+        //dump($this->container->getParameter('doctrine'));
+        die();
         //$this->container->getParameter('doctrine.default_connection');
-        
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('Resources/config/services.xml');
+
+        $container->services()
+            ->set('yamous_inspect_database.show_database_command', ShowDatabaseCommand::class)
+                ->args([service('doctrine')]);
+
+        //$definition = $builder->getDefinition('yamous_inspect_database.show_database_command');
+        //$definition->setArgument(0, $builder->get('doctrine.orm.default_entity_manager'));
+        //$definition->setArgument(0, $builder->getParameter('doctrine.default_connection'));
     }
 
     public function prependExtension(ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder): void
     {
-        $loader = new XmlFileLoader($containerBuilder, new FileLocator(__DIR__.'/Resources/config'));
-        $loader->load('services.xml');
+        //$loader = new XmlFileLoader($containerBuilder, new FileLocator(__DIR__.'/Resources/config'));
+        //$loader->load('services.xml');
     }
 }
