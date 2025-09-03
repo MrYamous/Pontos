51c1c78024b6444a159bd82d58c343db41362c44
MrYamous <matt.lempereur@gmail.com>
2025-09-03 21:47:44 +0200

    fix: bundle configuration

--- src/InspectDatabaseBundle.php
+++ src/InspectDatabaseBundle.php
@@ -2,7 +2,6 @@
 
 namespace Yamous\InspectDatabaseBundle;
 
-use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -12,30 +11,15 @@ use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 final class InspectDatabaseBundle extends AbstractBundle
 {
-    public function boot(): void
-    {
-        dump($this->container->get('doctrine.orm.default_entity_manager'));
-        //dump($this->container->getParameter('doctrine'));
-        die();
-        //$this->container->getParameter('doctrine.default_connection');
-    }
-
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
         $container->import('Resources/config/services.xml');
 
         $container->services()
             ->set('yamous_inspect_database.show_database_command', ShowDatabaseCommand::class)
-                ->args([service('doctrine')]);
-
-        //$definition = $builder->getDefinition('yamous_inspect_database.show_database_command');
-        //$definition->setArgument(0, $builder->get('doctrine.orm.default_entity_manager'));
-        //$definition->setArgument(0, $builder->getParameter('doctrine.default_connection'));
-    }
-
-    public function prependExtension(ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder): void
-    {
-        //$loader = new XmlFileLoader($containerBuilder, new FileLocator(__DIR__.'/Resources/config'));
-        //$loader->load('services.xml');
+            ->args([
+                service('doctrine.orm.default_entity_manager'),
+            ])
+            ->tag('console.command');
     }
 }
