1eb05ec0eed104f5ae3849696c693ea2f82a31fc
MrYamous <matt.lempereur@gmail.com>
2025-09-09 20:58:52 +0200

    feat: init tb:show command

---
+++ src/Command/ShowTableCommand.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Yamous\InspectDatabaseBundle\Command;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Attribute\Argument;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(name: 'table:show', aliases: ['tb:show'])]
+final class ShowTableCommand extends Command
+{
+    public function __construct(public EntityManagerInterface $entityManager) {
+        parent::__construct();
+    }
+
+    public function __invoke(
+        InputInterface $input,
+        OutputInterface $output,
+        #[Argument] string $tableName): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        
+        $connection = $this->entityManager->getConnection();
+        $schemaManager = $connection->createSchemaManager();
+
+        $columns = $schemaManager->listTableDetails($tableName)->getColumns();
+
+        $tableData = array_map(
+            fn($column) => [
+                $column->getName(),
+                $column->getType()->getName(),
+                $column->getUnsigned() ? 'Yes' : 'No',
+                $column->getFixed() ? 'Yes' : 'No',
+                $column->getLength() ?? '-',
+                $column->getPrecision() ?? '-',
+                $column->getScale() ?? '-',
+                $column->getNotnull() ? 'No' : 'Yes',
+                $column->getDefault() ?? '-',
+                $column->getAutoincrement() ? 'Yes' : 'No',
+                $column->getComment() ?? '-'
+            ],
+            $columns
+        );
+
+        $io->section('Columns Information');
+
+        $io->table([
+            'Name',
+            'Type',
+            'Unsigned',
+            'Fixed',
+            'Length',
+            'Precision',
+            'Scale',
+            'Nullable',
+            'Default',
+            'Autoincrement',
+            'Comment'
+        ],
+            $tableData
+        );
+
+        return Command::SUCCESS;
+    }
+}
--- src/InspectDatabaseBundle.php
+++ src/InspectDatabaseBundle.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Yamous\InspectDatabaseBundle\Command\ShowDatabaseCommand;
+use Yamous\InspectDatabaseBundle\Command\ShowTableCommand;
 
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
@@ -17,6 +18,11 @@ final class InspectDatabaseBundle extends AbstractBundle
 
         $container->services()
             ->set('yamous_inspect_database.show_database_command', ShowDatabaseCommand::class)
+            ->args([
+                service('doctrine.orm.default_entity_manager'),
+            ])
+            ->tag('console.command')
+            ->set('yamous_inspect_database.show_table_command', ShowTableCommand::class)
             ->args([
                 service('doctrine.orm.default_entity_manager'),
             ])
